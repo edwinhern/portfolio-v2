537695d21144360051bf8232415e0932c56b6377
Edwin Hernandez <edwinhern.16@gmail.com>
2024-04-03 00:41:34 -0500

    ci: :package: Add @vercel/analytics package

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { PropsWithChildren } from 'react';
 
@@ -10,6 +11,7 @@ export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
     <ThemeProvider>
       {children}
       <SpeedInsights />
+      <Analytics />
     </ThemeProvider>
   );
 };
