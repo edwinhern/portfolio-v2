23f64973d247b550c30b34fe89c66b1c8a6a4d9a
Edwin Hernandez <edwinhern.16@gmail.com>
2024-04-06 03:05:52 -0500

    refactor: :package: Update Analytics import in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css';
 
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Poppins, Raleway } from 'next/font/google';
