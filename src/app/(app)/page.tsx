ff33f716d613751309175b81491557744d9f93f0
Edwin Hernandez <edwinhern.16@gmail.com>
2024-04-06 15:52:55 -0500

    feat: :mag: Add metadata configurations

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,36 +1,32 @@
 import Link from 'next/link';
 
-import { Experience } from '@/components/experience';
-import { Projects } from '@/components/projects';
-import { Stack } from '@/components/stack';
 import { buttonVariants } from '@/components/ui/button';
 import { Icons } from '@/components/ui/icons';
 import { PageActions, PageHeader, PageHeaderDescription, PageHeaderHeading } from '@/components/ui/page-header';
+import { siteConfig } from '@/config/site';
 import { cn } from '@/lib/utils';
 
 export default function Home() {
   return (
-    <main className="container relative min-h-lvh">
+    <div className="container relative">
       <PageHeader>
-        <PageHeaderHeading>Build your component library</PageHeaderHeading>
-        <PageHeaderDescription>
-          Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open
-          Source.
-        </PageHeaderDescription>
+        <PageHeaderHeading>My Name is Edwin Hernandez .</PageHeaderHeading>
+        <PageHeaderDescription>{siteConfig.descriptions}</PageHeaderDescription>
         <PageActions>
-          <Link href="/docs" className={cn(buttonVariants())}>
+          <Link href="/" className={cn(buttonVariants())}>
             Get Started
           </Link>
-          <Link target="_blank" rel="noreferrer" href={'/'} className={cn(buttonVariants({ variant: 'outline' }))}>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.github}
+            className={cn(buttonVariants({ variant: 'outline' }))}
+          >
             <Icons.gitHub className="mr-2 size-4" />
             GitHub
           </Link>
         </PageActions>
       </PageHeader>
-
-      <Experience />
-      <Projects />
-      <Stack />
-    </main>
+    </div>
   );
 }
