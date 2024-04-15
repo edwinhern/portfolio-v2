f3a5a5c9c2757e7c0c59e26b305eabee641dbace
Edwin Hernandez <edwinhern.16@gmail.com>
2024-04-14 22:09:57 -0500

    fix: :white_check_mark: Fix test issues in vitest.setup.ts

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,6 +2,31 @@ import '@testing-library/jest-dom';
 
 import { cleanup } from '@testing-library/react';
 
+// Mock IntersectionObserver
+export class IntersectionObserver {
+  root = null;
+  rootMargin = '';
+  thresholds = [];
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return [];
+  }
+
+  unobserve() {
+    return null;
+  }
+}
+window.IntersectionObserver = IntersectionObserver;
+global.IntersectionObserver = IntersectionObserver;
+
 afterEach(() => {
   cleanup();
 });
