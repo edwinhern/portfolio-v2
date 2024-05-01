5bfdec47a338523179208a12aed1bde8ff4e6431
Edwin Hernandez <edwinhern.16@gmail.com>
2024-05-01 00:14:16 -0700

    refactor: :recycle: Refactor projects.ts and index.d.ts files

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,6 @@
-// *API*
-// Github
-
+/**
+ * @description This type is associated with the GitHub pinned repositories endpoint
+ **/
 type GithubRepo = {
   description: string;
   forks: number;
@@ -12,60 +12,4 @@ type GithubRepo = {
   repo: string;
   stars: number;
   website: string;
-};
-
-// Wakatime
-type Languages = {
-  decimal: string;
-  digital: string;
-  hours: number;
-  minutes: number;
-  name: string;
-  percent: number;
-  text: string;
-  total_seconds: number;
-};
-
-// Discord
-type DiscordUser = {
-  avatar: string;
-  global_name: string;
-  id: string;
-  username: string;
-};
-
-type Status = 'dnd' | 'idle' | 'offline' | 'online' | string;
-
-type Activity = {
-  application_id?: string;
-  assets?: {
-    large_image?: string;
-    large_text?: string;
-    small_image?: string;
-    small_text?: string;
-  };
-  details?: string;
-  name: string;
-  state?: string;
-  timestamps?: {
-    end?: number;
-    start?: number;
-  };
-};
-
-type DiscordApiContent = {
-  activities: Activity[];
-  discord_status: string;
-  discord_user: DiscordUser;
-  spotify: {
-    album_art_url: string;
-  };
-} & DiscordKeyString;
-
-type DiscordKeyString = {
-  [key: string]: DiscordApiContent;
-};
-
-type DiscordApiResponse = {
-  data: DiscordApiContent;
 };
