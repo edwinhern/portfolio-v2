aaf421b0bae7e550c9221ad291db74e364cf6490
Edwin Hernandez <edwinhern.16@gmail.com>
2024-05-01 09:26:53 -0700

    refactor: :recycle: Refactor experience and projects components

--- src/components/stack.tsx
+++
@@ -1,9 +0,0 @@
-export function Stack() {
-  return (
-    <div className="min-h-dvh py-16 sm:py-24">
-      <h1 className="scroll-m-20" id="stack-section">
-        Stack
-      </h1>
-    </div>
-  );
-}
