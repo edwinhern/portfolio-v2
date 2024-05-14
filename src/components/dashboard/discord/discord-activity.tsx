8bc66e84694e5b115d4209fb03fcc09972307d5d
Edwin Hernandez <edwinhern.16@gmail.com>
2024-05-14 14:47:04 -0500

    feat: :children_crossing: Add Reveal animation to Discord Activity component

--- src/components/dashboard/discord/discord-activity.tsx
+++ src/components/dashboard/discord/discord-activity.tsx
@@ -8,6 +8,7 @@ import { Activity, useLanyardWS } from 'use-lanyard';
 import { RenderIf } from '@/components/common/render-if';
 import { Alert, AlertDescription } from '@/components/ui/alert';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Reveal } from '@/components/ui/reveal';
 import { Skeleton } from '@/components/ui/skeleton';
 
 import { DiscordActivityCard } from './discord-activity-card';
@@ -38,33 +39,42 @@ export const DiscordActivity = () => {
   const lanyard = useLanyardWS(`${BigInt(env.NEXT_PUBLIC_DISCORD_ID || 1)}`);
 
   return (
-    <Card className="w-full">
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-base font-medium">Discord activity</CardTitle>
-      </CardHeader>
+    <Reveal
+      className="relative flex flex-1"
+      initial="visible"
+      whileHover={{
+        scale: 1.02,
+        transition: { duration: 0.4, ease: 'easeInOut' },
+      }}
+    >
+      <Card className="w-full">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-base font-medium">Discord activity</CardTitle>
+        </CardHeader>
 
-      <CardContent className="flex flex-col gap-4">
-        {/* Render if no data is present */}
-        <RenderIf when={!lanyard}>
-          <NoDataSkeleton />
-        </RenderIf>
-
-        <RenderIf when={Boolean(lanyard)}>
-          <DiscordStatus data={lanyard as Data} />
-
-          {/* Render if no activities */}
-          <RenderIf when={!lanyard?.activities?.length}>
-            <Alert className="border-none bg-primary">
-              <AlertDescription>No activities currently.</AlertDescription>
-            </Alert>
+        <CardContent className="flex flex-col gap-4">
+          {/* Render if no data is present */}
+          <RenderIf when={!lanyard}>
+            <NoDataSkeleton />
           </RenderIf>
 
-          {/* Render activities */}
-          <RenderIf when={Boolean(lanyard?.activities?.some((a) => a.name !== 'Custom Status'))}>
-            <ActivityFeed activities={lanyard?.activities || []} lanyard={lanyard as Data} />
+          <RenderIf when={Boolean(lanyard)}>
+            <DiscordStatus data={lanyard as Data} />
+
+            {/* Render if no activities */}
+            <RenderIf when={!lanyard?.activities?.length}>
+              <Alert className="border-none bg-primary">
+                <AlertDescription>No activities currently.</AlertDescription>
+              </Alert>
+            </RenderIf>
+
+            {/* Render activities */}
+            <RenderIf when={Boolean(lanyard?.activities?.some((a) => a.name !== 'Custom Status'))}>
+              <ActivityFeed activities={lanyard?.activities || []} lanyard={lanyard as Data} />
+            </RenderIf>
           </RenderIf>
-        </RenderIf>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Reveal>
   );
 };
