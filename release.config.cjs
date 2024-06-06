852c7ab91d32aa1b2e02f487897a55aa09f39082
Edwin Hernandez <edwinhern.16@gmail.com>
2024-06-06 18:44:21 -0500

    chore: :heavy_minus_sign: Remove @semantic-release/git from devDependencies

--- release.config.cjs
+++ release.config.cjs
@@ -7,12 +7,5 @@ module.exports = {
     '@semantic-release/commit-analyzer', // Analyzes your commit messages and determines the type of version bump
     '@semantic-release/release-notes-generator', // Generates release notes based on commit messages
     '@semantic-release/github', // Creates a GitHub release
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-        message: 'chore(release): :bookmark: bump version ${nextRelease.version}',
-      },
-    ],
   ],
 };
