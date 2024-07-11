b05c5d3554bcf744326a109fabdbda8ddc2698a6
Edwin Hernandez <edwinhern.16@gmail.com>
2024-07-11 13:59:56 -0500

    refactor: :recycle: Remove test logic/packages and switch to use biomejs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,20 @@
-import million from 'million/compiler';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizePackageImports: ['framer-motion'],
-    webVitalsAttribution: ['FCP', 'LCP', 'CLS', 'FID', 'TTFB', 'INP'],
+    optimizePackageImports: ["framer-motion"],
+    webVitalsAttribution: ["FCP", "LCP", "CLS", "FID", "TTFB", "INP"],
   },
   images: {
     deviceSizes: [390, 435, 768, 1024, 1280],
-    formats: ['image/avif'],
+    formats: ["image/avif"],
     remotePatterns: [
-      { hostname: 'cdn.discordapp.com' },
-      { hostname: 'media.discordapp.net' },
-      { hostname: 'dcdn.dstn.to' },
+      { hostname: "cdn.discordapp.com" },
+      { hostname: "media.discordapp.net" },
+      { hostname: "dcdn.dstn.to" },
     ],
   },
   reactStrictMode: true,
   trailingSlash: false,
 };
 
-export default million.next(nextConfig);
+export default nextConfig;
